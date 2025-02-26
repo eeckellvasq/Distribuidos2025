44a168a75e66fdbcf74fafb34c1cb90072d7fc7b
eeckellvasq <eeckellvasq@alumni.unav.es>
2025-02-26 18:45:15 -0300

    Update factorial.m

--- factorial.m
+++ factorial.m
@@ -1,16 +1,24 @@
 clear all
 clc
 
-num=input('Introduzca un número entero:);
+num = input('Introduzca un número entero: ');
 
-factorial=1
+factorial = 1;
 
-for i=1:num-1
-    factorial=factorial*i
+for i = 1:num
+    factorial = factorial * i;
 end
 
-if num>=0
-    disp('El factorial de' num2str(num) 'es:' factorial)
+if num >= 0
+    disp(['El factorial de ', num2str(num), ' es: ', num2str(factorial)]);
 else
-    disp(['El factorial de',num,'es:',factorial])
+    disp(['El factorial de ', num2str(num), ' es: ', num2str(factorial)]);
 end
+
+% Errores:
+% 1. Falta de cierre en la cadena de input.
+% 2. Inicialización incorrecta de factorial.
+% 3. Rango incorrecto en el bucle.
+% 4. Falta de punto y coma en la multiplicación dentro del bucle.
+% 5. Uso incorrecto de disp con múltiples argumentos.
+% 6. Intento de concatenar un número sin conversión a string.
